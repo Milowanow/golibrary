c6f5529162638194dbf780dd98156064293b082e
rikitikitavi <milowanow.anton@gmail.com>
2022-05-13 02:53:03 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Milowanow/golibrary
+
+go 1.18
---
+++ golibrary.go
@@ -0,0 +1,7 @@
+package  golibrary
+
+import "fmt"
+
+func Hi(name string) string {
+	return fmt.Sprintf("Hi from golibrary, %s", name)
+}
